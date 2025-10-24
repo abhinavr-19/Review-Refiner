0aba20661444bcd58e71d93fd882030a1aebd0d3
Abhinav R <abhinavkichu67@gmail.com>
2025-10-24 18:34:45 +0000

    move the footer text slightly down

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 mt-[-2rem] flex justify-center items-center text-center">
+    <footer className="w-full py-4 flex justify-center items-center text-center">
       <div className="text-sm text-muted-foreground">
         Created by{' '}
         <a
