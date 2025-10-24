8edc451a3748e577b7b09b1d91240118b2f4b497
Abhinav R <abhinavkichu67@gmail.com>
2025-10-24 18:10:04 +0000

    move the footer slightly upwards

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="w-full py-6 px-4 sm:px-8 md:px-12 lg:px-24 bg-transparent border-none">
+    <footer className="w-full py-6 px-4 sm:px-8 md:px-12 lg:px-24 bg-transparent border-none -mt-12">
       <div className="text-center text-sm text-muted-foreground">
         Created by{' '}
         <Link
