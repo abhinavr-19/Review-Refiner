7e425281269defd19022a5b7695a066b5615a158
Abhinav R <abhinavkichu67@gmail.com>
2025-10-24 18:08:25 +0000

    remove the box in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="w-full py-6 px-4 sm:px-8 md:px-12 lg:px-24">
+    <footer className="w-full py-6 px-4 sm:px-8 md:px-12 lg:px-24 bg-transparent border-none">
       <div className="text-center text-sm text-muted-foreground">
         Created by{' '}
         <Link
